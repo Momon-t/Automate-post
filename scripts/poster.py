c2d76b6634a9ba569a509d33de7c03fb663bfab8
Momon-t <momontaruto215@gmail.com>
2025-06-11 17:52:19 +0900

    Update poster.py

--- scripts/poster.py
+++ scripts/poster.py
@@ -3,7 +3,8 @@ from datetime import datetime
 from playwright.sync_api import sync_playwright
 import os
 
-csv_path = "../data/posts.csv"
+from pathlib import Path
+csv_path = Path("data") / "posts.csv"
 now = datetime.now().replace(second=0, microsecond=0)
 
 def find_matching_post():
